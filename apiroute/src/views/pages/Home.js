ee3fac37695cd0f6517164aae760e0d0e9329bb0
Reetik <reetik.raj.chy17@itbhu.ac.in>
2020-10-07 11:42:13 +0530

    for customary pull request

--- apiroute/src/views/pages/Home.js
+++ apiroute/src/views/pages/Home.js
@@ -1,7 +1,8 @@
 let Home = {
     render : async () => {
+    	/*I am writing this for a customary pull request. All the code is in my master branch*/
         let view = /*html*/`
-        Welcome to the routing app!!!
+        Welcome to the routing app!!! 
         `
         return view
     }
