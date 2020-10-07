7f5eddef4f110072b89a7955979032f45e16322e
Reetik <reetik.raj.chy17@itbhu.ac.in>
2020-10-07 11:38:18 +0530

    all crud functionalities working

---
+++ apiroute/src/views/pages/Home.js
@@ -0,0 +1,11 @@
+let Home = {
+    render : async () => {
+        let view = /*html*/`
+        Welcome to the routing app!!!
+        `
+        return view
+    }
+    , after_render: async () => { }
+}
+
+export default Home;
